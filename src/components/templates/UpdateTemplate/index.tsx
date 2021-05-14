510d8bd359abe6a384b70ef1e01ac1b7af8b1ed1
leee1846 <leee1846@gmail.com>
2021-05-14 11:51:33 +0900

    add:마이페이지 설정 예상 컴포넌트 분리

---
+++ src/components/templates/UpdateTemplate/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+const UpdateTemplate = (): ReactElement => {
+  return (
+    <div>
+      <p>모큘러스</p>
+      <p>오가니즘</p>
+      <p>모큘러스</p>
+    </div>
+  );
+};
+
+export default UpdateTemplate;
---
+++ src/pages/myPages/update/UpdatePage.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import UpdateTemplate from '../../../components/templates/UpdateTemplate';
+
+const UpdatePage = (): ReactElement => {
+  return (
+    <>
+      <UpdateTemplate />
+    </>
+  );
+};
+
+export default UpdatePage;

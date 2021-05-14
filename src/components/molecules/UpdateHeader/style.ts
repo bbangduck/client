a3e19e804ff1e998f3ac89a88fbf0326353159c8
leee1846 <leee1846@gmail.com>
2021-05-14 11:56:19 +0900

    style: 설정 헤더 css

---
+++ src/components/molecules/UpdateHeader/style.ts
@@ -0,0 +1,38 @@
+import Styled from 'styled-components';
+
+export const Header = Styled.header`
+  position:relative;
+  height:56px;
+  width:100%;
+  display:flex;
+  align-items:center;
+  margin-bottom:20px;
+`;
+
+export const ImgBox = Styled.div`
+  width: 24px;
+  height: 24px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  cursor:pointer;
+  z-index:1;
+  `;
+
+export const H1Box = Styled.div`
+  width:100%;
+  height:56px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  position:absolute;
+  z-index:0;
+`;
+
+export const H1 = Styled.h1`
+  font-size: ${({ theme }) => theme.sizes.font[2]};
+  font-weight: bold;
+  line-height: 1.43;
+  letter-spacing: -0.35px;
+  color: ${({ theme }) => theme.colors.fontGray[1]};
+`;
--- src/components/templates/UpdateTemplate/index.tsx
+++ src/components/templates/UpdateTemplate/index.tsx
@@ -1,9 +1,10 @@
 import React, { ReactElement } from 'react';
+import UpdateHeader from '../../molecules/UpdateHeader';
 
 const UpdateTemplate = (): ReactElement => {
   return (
     <div>
-      <p>모큘러스</p>
+      <UpdateHeader content="설정" />
       <p>오가니즘</p>
       <p>모큘러스</p>
     </div>

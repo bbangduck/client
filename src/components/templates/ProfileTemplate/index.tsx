6edd42c03e1e16a4ad4a4cc38458aaf678ee322a
leee1846 <leee1846@gmail.com>
2021-05-14 12:28:37 +0900

    프로필 설정 예상 컴포넌트 분리

---
+++ src/components/templates/ProfileTemplate/index.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+
+const ProfileTemplate = (): ReactElement => {
+  return (
+    <div>
+      <p>모큘러스</p>
+      <p>모큘러스</p>
+      <p>오가니즘</p>
+    </div>
+  );
+};
+
+export default ProfileTemplate;

7da0f84f99c940485da09103339c20b9bfecbbd5
leee1846 <leee1846@gmail.com>
2021-05-14 16:50:00 +0900

    공지사항 페이지 예상 컴포넌트

---
+++ src/components/templates/NoticeTemplate/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+
+const NoticeTemplate = (): ReactElement => {
+  return (
+    <section>
+      <p>모큘러스</p>
+      <p>오가니즘</p>
+    </section>
+  );
+};
+
+export default NoticeTemplate;

33ccc64ad37abf6b9b297294f9a40b849aae181a
leee1846 <leee1846@gmail.com>
2021-05-14 12:41:48 +0900

    style: 프로필 설정 상단 이미지 css

---
+++ src/components/molecules/ProfileImg/style.ts
@@ -0,0 +1,21 @@
+import Styled from 'styled-components';
+
+export const Container = Styled.div`
+  width:80px;
+  margin:0 auto;
+  height:80px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  margin-bottom:20.4px;
+  position:relative;
+`;
+
+export const IconBox = Styled.div`
+  width:32px;
+  height:32px;
+  position:absolute;
+  right:0;
+  bottom:0;
+  cursor:pointer;
+`;
--- src/components/templates/ProfileTemplate/index.tsx
+++ src/components/templates/ProfileTemplate/index.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
+import ProfileImg from '../../molecules/ProfileImg';
+import UpdateHeader from '../../molecules/UpdateHeader';
 
 const ProfileTemplate = (): ReactElement => {
   return (
     <div>
-      <p>모큘러스</p>
-      <p>모큘러스</p>
+      <UpdateHeader content="프로필 설정" />
+      <ProfileImg />
       <p>오가니즘</p>
     </div>
   );

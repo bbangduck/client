e0a9f1f596c2dc4791154616224c64d935c4f4d1
leee1846 <leee1846@gmail.com>
2021-05-24 14:54:18 +0900

    feat: 엑세스토큰 만료시 재발급

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,18 +2,55 @@ import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_URL;
 
-export const axiosAPI = axios.create({
+const axiosAPI = axios.create({
   baseURL,
 });
 
-axiosAPI.interceptors.request.use((config) => {
-  const configResult = config;
+axiosAPI.interceptors.request.use(
+  (config) => {
+    const configResult = config;
+    const accessToken = localStorage.getItem('bbangAT');
 
-  if (localStorage.getItem('bbangAT')) {
-    const headers = { 'X-AUTH-TOKEN': localStorage.getItem('bbangAT') };
+    if (localStorage.getItem('bbangAT')) {
+      const headers = { 'X-AUTH-TOKEN': accessToken };
+      configResult.headers = headers;
+    }
 
-    configResult.headers = headers;
-  }
+    return configResult;
+  },
+  (error) => Promise.reject(error),
+);
 
-  return configResult;
-});
+axiosAPI.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    const originalRequest = config;
+
+    if (status === 401) {
+      const refreshToken = localStorage.getItem('bbangRT');
+
+      axios({
+        method: 'post',
+        url: '/api/auth/refresh',
+        headers: { refreshToken },
+      }).then((response) => {
+        const accessTokens = response.data.accessToken;
+        const accessToken = accessTokens.header.concat(accessTokens.payload, accessTokens.signature);
+
+        localStorage.removeItem('bbangAT');
+        localStorage.setItem('bbangAT', accessToken);
+        originalRequest.headers = { 'X-AUTH-TOKEN': accessToken };
+        return axios(originalRequest);
+      });
+    }
+  },
+);
+
+export default axiosAPI;
--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,4 +1,4 @@
-import { axiosAPI } from './axios';
+import axiosAPI from './axios';
 
 const fetcher = (url: string): Promise<string> =>
   axiosAPI.get(url, { withCredentials: true }).then((response) => response.data);

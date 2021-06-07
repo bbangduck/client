7d7e7857cee9b4b2a2811b80e0c3cad7aa3d3133
leee1846 <leee1846@gmail.com>
2021-06-07 14:22:27 +0900

    style: 프로필설정-모달창 스타일 수정

--- src/components/organisms/ProfileList/index.tsx
+++ src/components/organisms/ProfileList/index.tsx
@@ -8,12 +8,20 @@ import { useClickOutside } from '../../../hooks/useClickOutside';
 import InputModal from '../../molecules/InPutModal';
 
 const UpdateList = (): ReactElement => {
-  const history = useHistory();
-
+  const [nickNameError, setNickNameError] = useState<{
+    message: string;
+    isError: boolean;
+  }>({ message: '', isError: false });
+  const [introError, setIntroError] = useState<{
+    message: string;
+    isError: boolean;
+  }>({ message: '', isError: false });
   const [nicknameValue, setNicknameValue] = useState('');
   const [introductionValue, setIntroductionValue] = useState('');
   const [nicknameModalRef, nicknameModalOn, setNicknameModalOn, nickNameClickOutside] = useClickOutside(false);
   const [introModalRef, introModalOn, setIntroModalOn, introClickOutside] = useClickOutside(false);
+
+  const history = useHistory();
   const userId = sessionStorage.getItem('bbangUserId');
 
   const onNicknameUpdate = async () => {
@@ -24,9 +32,15 @@ const UpdateList = (): ReactElement => {
         data: { nickname: nicknameValue },
       });
       setNicknameModalOn(false);
+      setNickNameError((prev) => {
+        return { ...prev, isError: false };
+      });
     } catch (err) {
       if (err.response.status === 400) {
-        alert('닉네임을 입력해주세요');
+        const message = '닉네임을 입력해주세요.';
+        setNickNameError(() => {
+          return { message, isError: true };
+        });
       } else {
         history.push('/error');
       }
@@ -35,16 +49,23 @@ const UpdateList = (): ReactElement => {
 
   const onIntroductionUpdate = async () => {
     try {
-      const res = await axiosAPI({
+      await axiosAPI({
         method: 'put',
         url: `/api/members/${userId}/descriptions`,
         data: { description: introductionValue || null },
       });
       setIntroModalOn(false);
-      console.log(res.status);
+      setIntroError((prev) => {
+        return { ...prev, isError: false };
+      });
     } catch (err) {
       if (err.response.status === 400) {
-        alert('자기소개를 입력해주세요');
+        const message = '나의 한마디를 입력해주세요.';
+        setIntroError(() => {
+          return { message, isError: true };
+        });
+      } else {
+        history.push('/error');
       }
     }
   };
@@ -67,6 +88,10 @@ const UpdateList = (): ReactElement => {
           setModalState={setNicknameModalOn}
           onUpdate={onNicknameUpdate}
           onValueChange={setNicknameValue}
+          title="닉네임을"
+          subTitle="닉네임"
+          inputError={nickNameError.isError}
+          errorMessage={nickNameError.message}
         />
       ) : null}
       {introModalOn ? (
@@ -76,6 +101,10 @@ const UpdateList = (): ReactElement => {
           setModalState={setIntroModalOn}
           onUpdate={onIntroductionUpdate}
           onValueChange={setIntroductionValue}
+          title="나의 한마디를"
+          subTitle="나의 한마디"
+          inputError={introError.isError}
+          errorMessage={introError.message}
         />
       ) : null}
     </div>

8915f34e20c51b67ac5a5d6ecdb835d0709c5703
leee1846 <leee1846@gmail.com>
2021-06-09 19:05:19 +0900

    fix: 내친구 userData 문장줄이기 util함수로 변경

--- src/components/molecules/UserData/index.tsx
+++ src/components/molecules/UserData/index.tsx
@@ -1,23 +1,17 @@
 import React, { ReactElement } from 'react';
 import image from '../../../assets/images/test/me.jpg';
+import { shortSentence } from '../../../hooks/shortSentence';
 import * as S from './style';
 
 const UserData = (): ReactElement => {
   const contents = '탠커/즐겜러/스토리 덕후입니다 1줄이상입니다';
-  const checkIntroLength = (content: string): string => {
-    if (content.length > 20) {
-      const newContent = `${content.substr(0, 20)}...`;
-      return newContent;
-    }
-    return content;
-  };
 
   return (
     <S.Containter>
       <S.ImgBox image={image} />
       <S.ContentBox>
         <S.Title>제로월드 강남점</S.Title>
-        <S.Intro>{checkIntroLength(contents)}</S.Intro>
+        <S.Intro>{shortSentence(20, contents)}</S.Intro>
         <S.Box>나와 5회 탈출</S.Box>
       </S.ContentBox>
     </S.Containter>

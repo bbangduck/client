f9cb075ccd08c1b739b76ee437c93c1630dd6e6a
leee1846 <leee1846@gmail.com>
2021-06-18 00:04:27 +0900

    style: 완료알림 스타일 적용

--- src/components/atoms/TopAlarm/style.ts
+++ src/components/atoms/TopAlarm/style.ts
@@ -4,12 +4,20 @@ interface ContainerProps {
   pop: boolean;
 }
 export const Container = Styled.div<ContainerProps>`
+  white-space:nowrap;
+  display:flex;
+  height:44px;
+  justify-content:center;
+  align-items:center;
   position:absolute;
   left:50%;
-  height:50px;
-  line-height:50px;
-  transform:translate(-50%,${({ pop }) => (pop ? '0' : ' -50px')});
+  padding: 12px 40px;
+  opacity: 0.7;
+  border-radius: 109px;
+  background-color: #000000;
   font-size: 14px;
-  color: #06880d;
+  letter-spacing: -0.14px;
+  color: #ffffff;
+  transform:translate(-50%,${({ pop }) => (pop ? '76px' : ' -80px')});
   transition:.5s;
 `;

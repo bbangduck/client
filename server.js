931028910926e15d9254443966f3f4dbb5464085
leee1846 <leee1846@gmail.com>
2021-06-19 20:35:40 +0900

    fix: 배포서버 코드수정

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express');
 const http = require('http');
-const bodyParser = require('body-parser');
 const path = require('path');
+
 const httpApp = express();
-httpApp.use(bodyParser.json());
-httpApp.use(bodyParser.urlencoded({
-  extended: true
-}));
-httpApp.all('/*', function (req, res, next) {
+
+httpApp.use(express.static(path.join(__dirname, './build')));
+
+httpApp.use(express.json());
+httpApp.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+httpApp.all('/*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
-httpApp.get('*', function (req, res) {
+
+httpApp.get('*', (req, res) => {
   // console.log(req);
-  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
 const httpServer = http.createServer(httpApp);
 
 httpServer.listen(80, () => {
-  console.log('HTTP Server running on port ' + 80);
+  console.log(`HTTP Server running on port ${80}`);
 });

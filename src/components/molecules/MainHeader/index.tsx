59d36261bad807c04dc4d971527a285df6b795cf
leee1846 <leee1846@gmail.com>
2021-06-19 20:38:51 +0900

    feat:로고 이미지추가

--- src/components/molecules/MainHeader/index.tsx
+++ src/components/molecules/MainHeader/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { useHistory } from 'react-router-dom';
 import search from '../../../assets/images/search/search.png';
+import logo from '../../../assets/images/logo/logoBlack.png';
 import * as S from './style';
 
 const MainHeader = (): ReactElement => {
@@ -8,7 +9,7 @@ const MainHeader = (): ReactElement => {
 
   return (
     <S.Header>
-      <p>빵덕</p>
+      <img src={logo} alt="로고" />
       <S.Img src={search} alt="찾기" onClick={() => history.push('/search')} data-blink="blink" />
     </S.Header>
   );

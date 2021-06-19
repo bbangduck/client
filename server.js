2d00c7bad4cff009f4a76dfa7c927876a5835712
Appleater <appleater7@gmail.com>
2021-06-19 18:18:11 +0900

    npm dependency 추가

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const path = require('path');
+const httpApp = express();
+httpApp.use(bodyParser.json());
+httpApp.use(bodyParser.urlencoded({
+  extended: true
+}));
+httpApp.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+httpApp.get('*', function (req, res) {
+  // console.log(req);
+  res.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
+
+const httpServer = http.createServer(httpApp);
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port ' + 80);
+});

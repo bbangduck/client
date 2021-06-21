fce9b220ec4e400e1d06a52c099698d15e6bc722
leee1846 <leee1846@gmail.com>
2021-06-21 12:04:46 +0900

    fix: 획득 배지 반응형 적용

--- src/components/templates/BedgeTemplate/index.tsx
+++ src/components/templates/BedgeTemplate/index.tsx
@@ -5,6 +5,7 @@ import BadgeList from '../../organisms/BedgeList';
 import left from '../../../assets/images/arrow/left.png';
 import userExist from '../../../utils/userExist';
 import useGetUserData from '../../../swr/useGetUserData';
+import * as S from './style';
 
 const BedgeTemplate = (): ReactElement => {
   const { errorStatus } = useGetUserData();
@@ -13,7 +14,9 @@ const BedgeTemplate = (): ReactElement => {
   if (!userExist() || withDrawalUser) return <Redirect to="/login" />;
   return (
     <section>
-      <UpdateHeader content="획득 배지" arrow={left} />
+      <S.Box>
+        <UpdateHeader content="획득 배지" arrow={left} />
+      </S.Box>
       <BadgeList />
     </section>
   );

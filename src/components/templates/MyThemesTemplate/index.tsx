ca810822a7823de6aa6f6803b114f6a7e99dc24b
leee1846 <leee1846@gmail.com>
2021-06-21 12:15:53 +0900

    fix: 관심테마 반응형적용

--- src/components/templates/MyThemesTemplate/index.tsx
+++ src/components/templates/MyThemesTemplate/index.tsx
@@ -5,6 +5,7 @@ import left from '../../../assets/images/arrow/left.png';
 import MyThemesList from '../../organisms/MyThemesList';
 import userExist from '../../../utils/userExist';
 import useGetUserData from '../../../swr/useGetUserData';
+import * as S from './style';
 
 const MyThemeTemplate = (): ReactElement => {
   const { errorStatus } = useGetUserData();
@@ -13,7 +14,9 @@ const MyThemeTemplate = (): ReactElement => {
   if (!userExist() || withDrawalUser) return <Redirect to="/login" />;
   return (
     <section>
-      <UpdateHeader arrow={left} content="관심테마" />
+      <S.Box>
+        <UpdateHeader arrow={left} content="관심테마" />
+      </S.Box>
       <MyThemesList />
     </section>
   );
---
+++ src/components/templates/MyThemesTemplate/style.ts
@@ -0,0 +1,5 @@
+import Styled from 'styled-components';
+
+export const Box = Styled.div`
+  padding:0 20px;
+`;
